ffcc1b414ca33271d140fce2c7c5e062633d9547
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2022-11-02 12:07:35 +0100

    Initial

---
+++ fighterGaem/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+
+// landscape as background, moves according to plaen direction
+// still plaen with different textures depending on direction
+// plaen shoot tracing rounds
+// enemies = missiles, fly sporadic
+// teleport to other side of map when reached end
+// texture for being hit/crash, explosion on missile death
+// dropping bombs on bases/airfields? some amount of bases, randomly scatted outside ceratin distance of each other
+// 
+
+
+
+
+
+while(!Raylib.WindowShouldClose())
+{
+
+
+
+
+}

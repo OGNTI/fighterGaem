4162ecb81ce84bd503036c288c6b54e26e6e9bd4
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2022-11-03 11:35:00 +0100

    basic_movement.2

--- fighterGaem/Program.cs
+++ fighterGaem/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Raylib_cs;
-using System.Numerics;
 
 // Initial Idea:
 // landscape as background, moves according to plaen direction
@@ -12,9 +11,8 @@ using System.Numerics;
 
 // wanted features:
 // not being able to do 180 degree turn, do two 90 degree (four 45 degree), which happens when holding s/d then pressing w/a
-// having the direction you're going take priorty over the opposite, basically if w is pressed while s is held then w take priorty but s was held before
+// having the direction you're going take priorty over the opposite, basically if w is pressed while s is held then w take priorty but s was held before, because of if statements
 // if all keys released, plaen keeps going the last direction it was going
-// smoother texture transistions
 
 
 Rectangle screen = new Rectangle(0, 0, 1000, 800);
@@ -27,20 +25,20 @@ Texture2D plane = Raylib.LoadTexture("img/FighterUp.png");
 Texture2D planeLeft = Raylib.LoadTexture("img/FighterLeft.png");
 Texture2D planeDown = Raylib.LoadTexture("img/FighterDown.png");
 Texture2D planeRight = Raylib.LoadTexture("img/FighterRight.png");
+Texture2D planeUpRight = Raylib.LoadTexture("img/FighterUpRight.png");
+Texture2D planeUpLeft = Raylib.LoadTexture("img/FighterUpLeft.png");
+Texture2D planeDownRight = Raylib.LoadTexture("img/FighterDownRight.png");
+Texture2D planeDownLeft = Raylib.LoadTexture("img/FighterDownLeft.png");
 Rectangle backRect = new Rectangle(screen.width / 2 - backG.width / 2, screen.height / 2 - backG.height / 2, backG.width, backG.height);
 Rectangle planeRect = new Rectangle(screen.width / 2 - plane.width / 2, screen.height / 2 - plane.height / 2, plane.width, plane.height);
 
 
 
-
 int speed = 5;
 string currentScene = "start";
-Vector2 position;
 
 while (!Raylib.WindowShouldClose())
 {
-    position = new Vector2(planeRect.x, planeRect.y);
-
     if (currentScene == "start")
     {
         backRect.y += speed;
@@ -70,34 +68,29 @@ while (!Raylib.WindowShouldClose())
             backRect.x -= speed;
         }
 
-        // Border
-        // if (backRect.x > )
-        // {
-
-        // }
-        // else if ()
-        // {
-
-        // }
-        // else if ()
-        // {
-
-        // }
-        // else if ()
-        // {
-
-        // }
-
-
     }
     else if (currentScene == "end")
     {
 
     }
 
-
-
-
+    // Border
+        if (backRect.x > 0)
+        {
+            backRect.x = screen.width - backG.width;
+        }
+        else if (backRect.x - screen.width < -backG.width)
+        {
+            backRect.x = 0;
+        }
+        if (backRect.y > 0)
+        {
+            backRect.y = screen.height - backG.height;
+        }
+        else if (backRect.y - screen.height < -backG.height)
+        {
+            backRect.y = 0;
+        }
 
 
 
@@ -114,19 +107,19 @@ while (!Raylib.WindowShouldClose())
     {
         if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && Raylib.IsKeyDown(KeyboardKey.KEY_A)) // --------------------- Diagonal
         {
-            Raylib.DrawTextureEx(plane, position, -45, 1f, Color.WHITE);
+            Raylib.DrawTexture(planeUpLeft, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && Raylib.IsKeyDown(KeyboardKey.KEY_D))
         {
-            Raylib.DrawTextureEx(plane, position, 45, 1f, Color.WHITE);
+            Raylib.DrawTexture(planeUpRight, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && Raylib.IsKeyDown(KeyboardKey.KEY_A))
         {
-            Raylib.DrawTextureEx(planeDown, position, 45, 1f, Color.WHITE);
+            Raylib.DrawTexture(planeDownLeft, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && Raylib.IsKeyDown(KeyboardKey.KEY_D))
         {
-            Raylib.DrawTextureEx(planeDown, position, -45, 1f, Color.WHITE);
+            Raylib.DrawTexture(planeDownRight, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) // ------------------------------------------------------- vertical / horizontal
         {

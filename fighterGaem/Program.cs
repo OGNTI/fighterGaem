b4576e3c3f3b7fb5384dd396b5cf4345de9ca0b5
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2022-11-23 14:42:12 +0100

    0.2

--- fighterGaem/Program.cs
+++ fighterGaem/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Raylib_cs;
+using System.Numerics;
 
 // Initial Idea:
 // landscape as background, moves according to plaen direction
@@ -19,7 +20,6 @@ Rectangle screen = new Rectangle(0, 0, 1000, 800);
 Raylib.InitWindow((int)screen.width, (int)screen.height, "Gaem");
 Raylib.SetTargetFPS(60);
 
-
 Texture2D backG = Raylib.LoadTexture("img/landscape.png");
 Texture2D plane = Raylib.LoadTexture("img/FighterUp.png");
 Texture2D planeLeft = Raylib.LoadTexture("img/FighterLeft.png");
@@ -32,16 +32,22 @@ Texture2D planeDownLeft = Raylib.LoadTexture("img/FighterDownLeft.png");
 Rectangle backRect = new Rectangle(screen.width / 2 - backG.width / 2, screen.height / 2 - backG.height / 2, backG.width, backG.height);
 Rectangle planeRect = new Rectangle(screen.width / 2 - plane.width / 2, screen.height / 2 - plane.height / 2, plane.width, plane.height);
 
-
+Texture2D projectile = Raylib.LoadTexture("img/tracer_1.png");
+Rectangle projectileRect = new Rectangle(planeRect.x, planeRect.y, projectile.width, projectile.height);
+int projectileSpeed = 10;
 
 int speed = 5;
 string currentScene = "start";
+Vector2 dir = Vector2.UnitX;
+
 
 while (!Raylib.WindowShouldClose())
 {
     if (currentScene == "start")
     {
         backRect.y += speed;
+        dir.Y = -1;
+        dir.X = 0;
 
         if (Raylib.IsKeyPressed(KeyboardKey.KEY_ENTER))
         {
@@ -51,21 +57,54 @@ while (!Raylib.WindowShouldClose())
     else if (currentScene == "game")
     {
         // Movement
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && Raylib.IsKeyDown(KeyboardKey.KEY_A))
         {
-            backRect.y += speed;
+            dir.Y = -1;
+            dir.X = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            dir.Y = -1;
+            dir.X = +1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        {
+            dir.Y = +1;
+            dir.X = -1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        {
+            dir.Y = +1;
+            dir.X = +1;
+        }
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+        {
+            dir.Y = -1;
+            dir.X = 0;
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
         {
-            backRect.y -= speed;
+            dir.Y = +1;
+            dir.X = 0;
         }
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
         {
-            backRect.x += speed;
+            dir.Y = 0;
+            dir.X = -1;
         }
         else if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
         {
-            backRect.x -= speed;
+            dir.Y = 0;
+            dir.X = +1;
+        }
+        backRect.y -= dir.Y * speed;
+        backRect.x -= dir.X * speed;
+
+        // attack
+        if (Raylib.IsKeyPressed(KeyboardKey.KEY_SPACE))
+        {
+            projectileRect.x += dir.X * projectileSpeed;
+            projectileRect.y += dir.Y * projectileSpeed;
         }
 
     }
@@ -75,22 +114,22 @@ while (!Raylib.WindowShouldClose())
     }
 
     // Border
-        if (backRect.x > 0)
-        {
-            backRect.x = screen.width - backG.width;
-        }
-        else if (backRect.x - screen.width < -backG.width)
-        {
-            backRect.x = 0;
-        }
-        if (backRect.y > 0)
-        {
-            backRect.y = screen.height - backG.height;
-        }
-        else if (backRect.y - screen.height < -backG.height)
-        {
-            backRect.y = 0;
-        }
+    if (backRect.x > 0)
+    {
+        backRect.x = screen.width - backG.width;
+    }
+    else if (backRect.x - screen.width < -backG.width)
+    {
+        backRect.x = 0;
+    }
+    if (backRect.y > 0)
+    {
+        backRect.y = screen.height - backG.height;
+    }
+    else if (backRect.y - screen.height < -backG.height)
+    {
+        backRect.y = 0;
+    }
 
 
 
@@ -105,37 +144,44 @@ while (!Raylib.WindowShouldClose())
     }
     else if (currentScene == "game")
     {
-        if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && Raylib.IsKeyDown(KeyboardKey.KEY_A)) // --------------------- Diagonal
+
+        if (dir.Y == -1 && dir.X == -1) // ----------------------------------------------------- Diagonal 
         {
             Raylib.DrawTexture(planeUpLeft, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_W) && Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        else if (dir.Y == -1 && dir.X == 1)
         {
             Raylib.DrawTexture(planeUpRight, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        else if (dir.Y == 1 && dir.X == -1)
         {
             Raylib.DrawTexture(planeDownLeft, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S) && Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        else if (dir.Y == 1 && dir.X == 1)
         {
             Raylib.DrawTexture(planeDownRight, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_W)) // ------------------------------------------------------- vertical / horizontal
+        else if (dir.Y == -1 && dir.X == 0) // ------------------------------------------------- vertical / horizontal
         {
             Raylib.DrawTexture(plane, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+        else if (dir.Y == 1 && dir.X == 0)
         {
             Raylib.DrawTexture(planeDown, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+        else if (dir.Y == 0 && dir.X == -1)
         {
             Raylib.DrawTexture(planeLeft, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
         }
-        else if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+        else if (dir.Y == 0 && dir.X == 1)
         {
             Raylib.DrawTexture(planeRight, (int)planeRect.x, (int)planeRect.y, Color.WHITE);
+        }
+
+        // attack
+        if (Raylib.IsKeyDown(KeyboardKey.KEY_SPACE))
+        {
+            Raylib.DrawTexture(projectile, (int)projectileRect.x, (int)projectileRect.y, Color.WHITE);
         }
 
     }
